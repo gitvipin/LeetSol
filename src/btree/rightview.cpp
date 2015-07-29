6dcaa54b51861d09c0c83b7560bbc2a5714ed883
gitvipin <sh.vipin@gmail.com>
2015-07-29 04:49:44 +0000

    Solution: find binary tree right view

--- src/btree/rightview.cpp
+++ src/btree/rightview.cpp
@@ -1,2 +1,27 @@
 // This program is the solution to following problem
 // https://leetcode.com/problems/binary-tree-right-side-view/
+
+void findR(TreeNode *root, int level, set<int> &mark, vector<int> & v)
+{
+    if(!root)
+        return;
+
+    if (mark.find(level) == mark.end()) {
+        v.push_back(root->val);
+        mark.insert(level);
+    }
+    findR(root->right, level+1, mark, v);
+    findR(root->left, level+1, mark, v);
+}
+
+
+
+
+vector<int> rightSideView(TreeNode* root) {
+    vector<int> v;
+    set<int>	mark;
+    findR(root, 0, mark, v);
+    return v;
+}
+
+
