b6482d8680da70391b37a4945c5220a9f51dc381
gitvipin <sh.vipin@gmail.com>
2015-07-29 04:50:50 +0000

    Find if there is a path with given sum in binary tree

---
+++ src/btree/pathsum.cpp
@@ -0,0 +1,21 @@
+// Problem : https://leetcode.com/problems/path-sum/
+
+bool hasPathSum(TreeNode* root, int sum) {
+    if (!root) {
+        return false;
+    }
+
+    if (!root->left && !root->right) {
+        if (root->val == sum)
+            return true;
+        else
+            return false;
+    }
+
+    if (hasPathSum(root->left, sum - root->val))
+        return true;
+    else
+        return hasPathSum(root->right, sum - root->val);
+}
+
+
