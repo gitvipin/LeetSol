c2ea275787a1f1d294aff361ad7bce091212226e
gitvipin <sh.vipin@gmail.com>
2015-08-06 15:36:01 +0000

    SOlution : Remove duplicates from sorted list

---
+++ src/list/removeDuplicates.cpp
@@ -0,0 +1,19 @@
+/*
+--------------------------------------------------------------------------------------
+Solution : Remove duplicates from Sorted Linked List
+https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+--------------------------------------------------------------------------------------
+*/
+ListNode* deleteDuplicates(ListNode* head) {
+    ListNode *ret = head;
+    while (head) {
+        if (head->next && head->val == head->next->val) {
+            ListNode *del = head->next;
+            head->next = del->next;
+            delete del;
+        } else {
+            head = head->next;
+        }
+    }
+    return ret;
+}
