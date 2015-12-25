ac2ae13ac772ff11f119af9c50ce3e8f77e26b1e
V S <sh.vipin@gmail.com>
2015-12-25 13:06:58 -0800

    Adding comment (TODO) for Valid parenthesis

--- src/Stack/ValidParenthesis.cpp
+++ src/Stack/ValidParenthesis.cpp
@@ -3,6 +3,19 @@
 Solution for : https://leetcode.com/problems/valid-parentheses/
 */
 
+/*
+Current solution is simple Stack based. 
+
+TODO : Can we assign weights ( prime numbers ) to 
+'(' - 2
+'{' - 3
+'{' - 5
+
+and add corresponding weight in total sum when the opening brace comes and subtract the same weight when the closing brace comes. 
+In the end, net sum should be zero. It solves the problem of 'presence' of matching parenthesis but it doesn't solve the problem
+of right order between them. e.g ."(])[" . How can we solve the second part of this problem ? If we can solve the second part as 
+well in this computation this solution will not require the O(n) space as in case of stack based solution. 
+*/
 bool isValid(string s) {
         std::stack<char> chk;
         const char *str = s.c_str();
