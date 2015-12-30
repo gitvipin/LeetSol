3846190fd240ec47d7c810b1e47e21b5bd1b29fa
V S <sh.vipin@gmail.com>
2015-12-30 01:31:03 -0800

    Update deleteNode.cpp

--- src/list/deleteNode.cpp
+++ src/list/deleteNode.cpp
@@ -3,6 +3,9 @@ Solution for Problem: https://leetcode.com/problems/delete-node-in-a-linked-list
 Delete any but last node in Linked List.
 */
 
+/*
+Copy the content of next node to this node and delete the next node.
+*/
 void deleteNode(ListNode* node) {
         if (!node || !node->next)
             return;
