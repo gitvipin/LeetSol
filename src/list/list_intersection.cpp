67f9fc4286ccb55c728cf072208548f1568fa0d7
V S <sh.vipin@gmail.com>
2015-12-30 01:48:51 -0800

    Adding comments for List Intersection.

--- src/list/list_intersection.cpp
+++ src/list/list_intersection.cpp
@@ -1,5 +1,24 @@
-//https://leetcode.com/problems/intersection-of-two-linked-lists/
+/*
+Solution for : https://leetcode.com/problems/intersection-of-two-linked-lists/
+*/
 
+/*
+There are multiple ways to solve this problem. 
+
+Soln (1) : Iterate over first list and for every element, check if it can be reached from second list. The first element that
+can be reached is the intersection point. If no element can be reached there is no intersection. 
+Time Complexity : O(n2) , Space Complexity : O(1)
+
+Soln (2) :  Use extra memory and Hash the nodes in first list. While iterating over the second if in hash table node already exists. 
+If node exists return the node which occurred first. Time : O(n+m) Space : O(m)
+
+Soln (3) : If two lists are of same length, then two lists will hit the common element at the same time. This solution works on this idea.
+Scan both lists, one node at a time. As soon as the shorter list exhausts, the longer one will have only the extra elements left
+as unvisited. Now advance head pointer of the larger list until second list is also exhausted. After this, headA and headB both will point to
+list of same lengths. Now scan again and return the node which is common between the two. 
+Time Complexity : O(n+m), Space Complexity : O(1)
+
+*/
 ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
     if (headA && headB) {
         ListNode *pA = headA;
