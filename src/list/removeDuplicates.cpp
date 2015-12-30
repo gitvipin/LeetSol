eefdf4bcb83ff7e243d9d479b93062e189fffd57
V S <sh.vipin@gmail.com>
2015-12-30 02:22:19 -0800

    Update removeDuplicates.cpp

--- src/list/removeDuplicates.cpp
+++ src/list/removeDuplicates.cpp
@@ -7,6 +7,7 @@ https://leetcode.com/problems/remove-duplicates-from-sorted-list/
 ListNode* deleteDuplicates(ListNode* head) {
     ListNode *ret = head;
     while (head) {
+        // must check for head->next to avoid segmentation fault.
         if (head->next && head->val == head->next->val) {
             ListNode *del = head->next;
             head->next = del->next;
