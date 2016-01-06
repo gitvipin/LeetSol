818e2b33f5533913cb6f09ecff5c29c32c888041
V S <sh.vipin@gmail.com>
2016-01-06 08:19:19 -0800

    Update hasCycle.cpp

--- src/list/hasCycle.cpp
+++ src/list/hasCycle.cpp
@@ -1,3 +1,23 @@
+
+/*
+Solution for : https://leetcode.com/problems/linked-list-cycle/
+*/
+
+/*
+This problem can be solved in multiple ways. 
+
+(1) Start traversing the Linked List. Keep hashing the nodes and if you find a node already hashed,
+you have a cycle in the linked list. If you reach NULL without conflict, there is no cycle. 
+Time Complexity : O(n)
+Space Complexity : O(n)
+
+
+(2) Use Fast pointer, slow pointer approach. Make a pointer run twice as fast as the other one, if the
+both meet at some point, there is a loop in list 
+Time Complexity : O(n)
+Space Complexity : O(1)
+*/
+
 bool hasCycle(ListNode *head) {
     if (!head)
         return false;
