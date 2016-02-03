08468d14c19b0e4b60cce01bd4f09281d781b397
V S <sh.vipin@gmail.com>
2016-02-02 23:05:15 -0800

    Update EmpMngrSalary.sql

--- db/sql/EmpMngrSalary.sql
+++ db/sql/EmpMngrSalary.sql
@@ -2,6 +2,10 @@
 Solution for : https://leetcode.com/problems/employees-earning-more-than-their-managers/
 */
 
+/*
+Best tutorial that explains this is 
+https://lagunita.stanford.edu/courses/DB/SQL/SelfPaced/courseware/ch-sql/seq-vid-table_variables_and_set_operators/ @ 3:44
+*/
 select E.Name
 from Employee E , Employee M
 where E.ManagerId = M.Id and E.Salary > M.Salary
