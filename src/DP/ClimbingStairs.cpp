f671c5a9a7d1827e01bf368b8b8f0b602874cb52
V S <sh.vipin@gmail.com>
2016-02-14 23:39:11 -0800

    Adding comment for Climbing Stairs

--- src/DP/ClimbingStairs.cpp
+++ src/DP/ClimbingStairs.cpp
@@ -2,9 +2,20 @@
 Solution for : https://leetcode.com/problems/climbing-stairs/
 */
 
+/*
+Solution to this problem is an elegant application of Fibonacci Series. 
+
+Basically at nth step you can come only two ways. First from (n-1)th step with 1 more step
+or Second is from (n-2)nd step wiht 2 steps at a time. It means number of ways one can 
+reach on nth step is sum of number of ways one can reach on (n-1) step and (n-2) step.
+
+Following solution encodes this only. 
+*/
 int climbStairs(int n) {
         if (n <= 2)
             return n;
+            
+        // We need only (n-1) and (n-2) step solution so keep only two variables.
         int prev=1, curr = 2;
         
         for (int x = 3 ; x <= n; x++) {
