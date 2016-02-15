0825d9fe8d49b757ee779b3caae2dcf9b7a8ede0
V S <sh.vipin@gmail.com>
2016-02-14 23:44:28 -0800

    Adding comment for House Robber

--- src/DP/HouseRobber.cpp
+++ src/DP/HouseRobber.cpp
@@ -2,6 +2,11 @@
 Solution for : https://leetcode.com/problems/house-robber/
 */
 
+/*
+Dynamic Programming offers a neat and clean solution for this problem. 
+At nth house, robber can check what gives more yield : Robbing this and previous alternate
+house or yield on immediate previous house.
+*/
  int rob(vector<int>& nums) {
         
         size_t count = nums.size();
@@ -18,8 +23,8 @@ Solution for : https://leetcode.com/problems/house-robber/
             return curr;
         
         for (size_t x = 2; x < count; x++) {
-            int max = std::max(prev + nums[x], curr);
-            prev = std::max(prev, curr);
+            int max = std::max(prev + nums[x], curr); // it calculates the max yield until xth house.
+            prev = std::max(prev, curr); // Also update the yield till previous house.
             curr = max;
         }
         return std::max(prev, curr);
